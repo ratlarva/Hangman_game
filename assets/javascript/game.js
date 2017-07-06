17fcc78495ae6227bdb6665aa12490cc495a68dc
unknown <ratlarva@gmail.com>
2017-07-06 13:45:23 -0600

    better js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,13 +12,36 @@ var targetDiv = document.getElementById("underScores");
 
 for (var i = 0; i < currentWord.length; i++) {
     
-      var newDiv = document.createElement("div");
+      /*var newDiv = document.createElement("div");
       console.log(newDiv);
       newDiv.innerHTML = "hello";
-      targetDiv.appendChild(newDiv);
+      targetDiv.appendChild(newDiv);*/
 
     }
+var letterPositions = [];
+var misses = 0;
+window.addEventListener('keyup',
+          function(event) {
+          	var guess = event.key;
 
-document.onkeyup = function(){
+          	
+          	for (var i = 0; i < currentWord.length; i++) {
+          		if (guess === currentWord.charAt(i)) {
+          			letterPositions.push(i);
+          			console.log(letterPositions);
+          			console.log(i);
+          		}
+          		else{
+          			misses++;
+          			if(misses === currentWord.length){
 
-}
+          			}
+          		}
+
+          	var check = $("<div>");
+          }
+          console.log(letterPositions[0]);
+          		console.log(letterPositions[1]);
+
+
+}, true);
